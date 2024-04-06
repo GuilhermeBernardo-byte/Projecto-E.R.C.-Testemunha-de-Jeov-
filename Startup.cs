73462fd1281a995ee82cea578770323e711201ca
Guilherme Bernardo <133515574+GuilhermeBernardo-byte@users.noreply.github.com>
2024-04-06 19:31:38 +0000

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PROJECTO_TLP.Startup))]
+namespace PROJECTO_TLP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
